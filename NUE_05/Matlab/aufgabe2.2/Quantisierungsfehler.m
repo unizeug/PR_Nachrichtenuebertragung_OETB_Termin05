ec2ca3180ed78614ef0d7ea8c48e2afe7ecd2968
özgü <oezgue.dogan@gmail.com>
2012-06-22 10:17:26 +0200

    weiß ich nicht mehr

--- NUE_05/Matlab/aufgabe2.2/Quantisierungsfehler.m
+++ NUE_05/Matlab/aufgabe2.2/Quantisierungsfehler.m
@@ -16,31 +16,64 @@ b = sinus100.B;
 a = a-mean(a);
 b = b-mean(b);
 
-%minimale zeitliche Verschiebung finden
-
 
 %und Signale entsprechend zuschneiden
 faktor = max(b)/max(a);
 a = a*faktor;
+% 
+% figure(1);
+% plot(a);
+% hold on
+% plot(b, 'r');
+% hold off
+% AXIS([0 3500 -1.5 2]);
+% xlabel('Zeitachse t');
+% ylabel('Amplitude V');
+% SUPTITLE(['\bf Eingangssignal und decodiertes, amplitudenangepasstes Signal',10]);
+% legend('Eingangssignal','decodiertes, amplitudenangepasstes Signal');
+% 
 
-figure(1);
-plot(a)
+%minimale zeitliche Verschiebung finden
+[xr,lag]=xcorr(a,b);
+[mx,mind]=max(abs(xr));
+delay_eva=lag(mind);
+
+% [c,lags] = xcorr(a,b);
+% index_center=(length(c)+1)./2;
+% [c,index_max]=max(c);
+% delay=index_max-index_center
+
+
+t = (1:length(a)-delay_eva);
+
+figure(2);
+plot(t,a(delay_eva+1:end))
 hold on
-plot(b, 'r')
+plot(t,b(1:end-delay_eva),'r')
 hold off
+AXIS([0 3500 -1.5 2]);
+xlabel('Zeitachse t');
+ylabel('Amplitude V');
+SUPTITLE(['\bf Eingangssignal und decodiertes, amplitudenangepasstes Signal ohne Zeitverschiebung',10]);
+legend('Eingangssignal','korrekt decodiertes Ausgangssignal');
 
-%Quantisierungsfehler bestimmen
-%y = Ergebnis decodiertes Signal, je auf Kanal A
-%u = Eingangsignal, je auf Kanal B
-a = y;
-b = u;
+%Quantisierungsfehler qf bestimmen
+y = a;          %y = Ergebnis decodiertes Signal, je auf Kanal A
+u = b;          %u = Eingangsignal, je auf Kanal B
 
-q = y-u;
+qf = y(delay_eva+1:end)-u(1:end-delay_eva);
+
+figure(3);
+plot(qf);
+% AXIS([0 3500 -1.5 2]);
+xlabel('Zeitachse t');
+ylabel('Amplitude V');
+SUPTITLE(['\bf Quantisierungsfehler',10]);
+
+% %Plot Quantisierungsfehler-Histogramm
+figure(4);
+hist(qf, 'g');     % macht automatisch 10 gleichgroﬂe bins
 
 
-%Plot Quantisierungsfehler-Histogramm
-%hist(q)     % macht automatisch 10 gleichgroﬂe bins
-
-
-%Plot Quantisierungsfehler-LDS
-
+% %Plot Quantisierungsfehler-LDS
+% 
