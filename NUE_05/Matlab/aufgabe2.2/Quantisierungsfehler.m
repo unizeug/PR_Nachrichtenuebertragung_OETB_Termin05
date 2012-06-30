07c9b8f29521b32f72d4010c92f52442a645369b
Boris Henckell <borishenckell@gmail.com>
2012-06-30 16:51:27 +0200

    Zeitachse bei dem Quantisierungsfehler eingefügt.
    fehler bei dem 8kHz signal gelöst

--- NUE_05/Matlab/aufgabe2.2/Quantisierungsfehler.m
+++ NUE_05/Matlab/aufgabe2.2/Quantisierungsfehler.m
@@ -1,20 +1,22 @@
-clear; close all;
+clear; close all;clc;
 
 %Messungen laden
 %Kanal B=Eingangssignal, Kanal A=decodiertes Signal
 
-%sinus8 = load('sinus_clk8kHz');
- sinus100 = load('sinus_clk100kHz');
-sinus8 = load('sinus_clk100kHz');
-% sinus100 = load('sinus_clk100kHz');
-% 
+sinus = load('sinus_clk8kHz');
+sinus.B = -1*sinus.B;
+
+%sinus = load('sinus_clk100kHz');
+
 % dreieck8 = load('dreieck_clk8kHz');
 % dreieck100 = load('dreieck_clk100kHz');
 
 %Mittelwertbefreiung
-a = sinus8.A;
-b = sinus8.B;
+a = sinus.A;
+b = sinus.B;
 
+% a(3710) = a(3720);     %Fuschkorrektur
+% a(1085) = -1.0480;    %Fuschkorrektur
 a = a-mean(a);
 b = b-mean(b);
 
@@ -23,12 +25,17 @@ b = b-mean(b);
 faktor = max(b)/max(a);
 a = a*faktor;
 
+
+tend=sinus.Length*sinus.Tinterval;
+t=linspace(0,tend,sinus.Length);
+
+
 figure(1);
-plot(a);
+plot(t,a);
 hold on
-plot(b, 'r');
+plot(t, b, 'r');
 hold off
-AXIS([0 3500 -1.5 2]);
+AXIS([0 0.05 -1.5 2]);
 xlabel('Zeitachse t');
 ylabel('Amplitude V');
 SUPTITLE(['\bf Eingangssignal und decodiertes, amplitudenangepasstes Signal',10]);
@@ -36,24 +43,29 @@ legend('Eingangssignal','decodiertes, amplitudenangepasstes Signal');
 
 
 %minimale zeitliche Verschiebung finden
-[xr,lag]=xcorr(a,b);
-[mx,mind]=max(abs(xr));
-delay_eva=lag(mind);
+% [xr,lag]=xcorr(a,b);
+% [mx,mind]=max(abs(xr));
+% delay_eva=lag(mind)
 
-% [c,lags] = xcorr(a,b);
-% index_center=(length(c)+1)./2;
-% [c,index_max]=max(c);
-% delay=index_max-index_center
+[c,lags] = xcorr(a,b);
+index_center=(length(c)+1)./2;
+[c,index_max]=max(c);
+delay=index_max-index_center
 
 
-t = (1:length(a)-delay_eva);
+% t = (1:length(a)-delay_eva);
+
+
+t = t(1:length(a)-delay);
 
 figure(2);
-plot(t,a(delay_eva+1:end))
+% plot(t,a(delay_eva+1:end))
+plot(t,a(delay+1:end))
 hold on
-plot(t,b(1:end-delay_eva),'r')
+% plot(t,b(1:end-delay_eva),'r')
+plot(t,b(1:end-delay),'r')
 hold off
-AXIS([0 3500 -1.5 2]);
+%AXIS([0 3500 -1.5 2]);
 xlabel('Zeitachse t');
 ylabel('Amplitude V');
 SUPTITLE(['\bf Eingangssignal und decodiertes, amplitudenangepasstes Signal ohne Zeitverschiebung',10]);
@@ -63,10 +75,11 @@ legend('Eingangssignal','korrekt decodiertes Ausgangssignal');
 y = a;          %y = Ergebnis decodiertes Signal, je auf Kanal A
 u = b;          %u = Eingangsignal, je auf Kanal B
 
-qf = y(delay_eva+1:end)-u(1:end-delay_eva);
+%qf = y(delay_eva+1:end)-u(1:end-delay_eva);
+qf = y(delay+1:end)-u(1:end-delay);
 
 figure(3);
-plot(qf);
+plot(t,qf);
 % AXIS([0 3500 -1.5 2]);
 xlabel('Zeitachse t');
 ylabel('Amplitude V');
@@ -74,8 +87,15 @@ SUPTITLE(['\bf Quantisierungsfehler',10]);
 
 % %Plot Quantisierungsfehler-Histogramm
 figure(4);
-hist(qf, 'g');     % macht automatisch 10 gleichgroï¿½e bins
-
+hist(qf);     % macht automatisch 10 gleichgroße bins
+xlabel('Spannung [V]');
+ylabel('Häufigkeit');
+SUPTITLE(['\bf Histogramm des Quantisierungsfehlers', 10]);
 
 % %Plot Quantisierungsfehler-LDS
 % 
+
+[qfauto,lag]=xcorr(qf,qf);
+
+
+%FFTshiftplotZP_autocorr
